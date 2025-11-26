a358797c9c0e002912c24b6300739541991830c6
thamizh <tamilselvan@daivtech.com>
2025-11-27 00:28:54 +0530

    removed cors

--- src/app.js
+++ src/app.js
@@ -6,33 +6,21 @@ const { errorHandler } = require("./middleware/errorHandler");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('=== INCOMING REQUEST ===');
-  console.log('Method:', req.method);
-  console.log('URL:', req.url);
-  console.log('Origin:', req.headers.origin);
-  console.log('User-Agent:', req.headers['user-agent']);
-  console.log('=======================');
-  next();
-});
+// const allowedOrigins = [
+//   "http://localhost:5173",
+//   "https://requestmaneger.vercel.app",
+// ];
 
-app.use(cors({
-  origin: true, 
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"]
-}));
-
-app.options('*', (req, res) => {
-  console.log('=== PREFLIGHT REQUEST ===');
-  console.log('Origin:', req.headers.origin);
-  res.header('Access-Control-Allow-Origin', req.headers.origin);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.status(204).send();
-});
-
+// app.use(
+//   cors({
+//     origin: true,
+//     origin: allowedOrigins,
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//     credentials: true,
+//   })
+// );
+// app.options("*", cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
