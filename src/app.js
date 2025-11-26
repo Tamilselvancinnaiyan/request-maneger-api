2bccfb75d0534533b0c15e508e4d2ab9cac3df44
thamizh <tamilselvan@daivtech.com>
2025-11-27 00:03:24 +0530

    updating cors domain

--- src/app.js
+++ src/app.js
@@ -8,7 +8,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://requestmaneger.vercel.app"
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
