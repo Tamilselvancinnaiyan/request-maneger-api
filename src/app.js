06982031f14dc10b6a9b0f211b811dd3034a1a7a
thamizh <tamilselvan@daivtech.com>
2025-11-27 00:40:37 +0530

    updating cors

--- src/app.js
+++ src/app.js
@@ -1,22 +1,26 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors"); 
 const routes = require("./routes");
 const { errorHandler } = require("./middleware/errorHandler");
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
-  
-  next();
-});
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://requestmaneger.vercel.app",
+  "https://requestmaneger.vercel.app/",
+];
 
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+app.options("*", cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
