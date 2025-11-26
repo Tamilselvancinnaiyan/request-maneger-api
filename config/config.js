b0d24b930e336da85f17418ecf9a25d3481743b0
thamizh <tamilselvan@daivtech.com>
2025-11-26 23:34:22 +0530

    add prod env

--- config/config.js
+++ config/config.js
@@ -15,5 +15,20 @@ module.exports = {
         rejectUnauthorized: false
       }
     }
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 };
