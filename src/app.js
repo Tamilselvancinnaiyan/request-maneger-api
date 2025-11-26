ef27f6192961bac7519ead16c1b14df2c29dce78
thamizh <tamilselvan@daivtech.com>
2025-11-27 00:31:02 +0530

    allowing all origin

--- src/app.js
+++ src/app.js
@@ -1,26 +1,22 @@
 const express = require("express");
 const morgan = require("morgan");
-const cors = require("cors"); 
 const routes = require("./routes");
 const { errorHandler } = require("./middleware/errorHandler");
 
 const app = express();
 
-// const allowedOrigins = [
-//   "http://localhost:5173",
-//   "https://requestmaneger.vercel.app",
-// ];
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  
+  next();
+});
 
-// app.use(
-//   cors({
-//     origin: true,
-//     origin: allowedOrigins,
-//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//     credentials: true,
-//   })
-// );
-// app.options("*", cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
